4641e91ac2ab154de559b925329166c7f70d95cd
goodnews <goodnewsj62@gmail.com>
2021-08-30 13:54:29 +0100

    basic setup

--- main/auth/auth.py
+++ main/auth/auth.py
@@ -10,9 +10,8 @@ from flask_mail import Message
 import jwt
 
 from .. import login_manager,create_app,bcrypt_,mail
-from .. import db as maindb
+from .. import db
 from ..oauth import OAuthSignIn
-from ..database import Database
 from .form import LoginForm,RegisterForm,ChangePassword
 from main.models import User,Post,Notification,follow
 
@@ -21,15 +20,13 @@ providers = ['twitter','google','facebook']
 
 auth = Blueprint('auth',__name__,url_prefix='/auth')
 
-#from database class to handle all db queries
-db = Database(User,Post,Notification,follow)
-
 @login_manager.user_loader
 def load_user(user_id):
-    return db.get_user(user_id)
+    return User.query.filter_by(id=user_id).first()
 
 #function blocks user logged in from accessing
 #the login page again
+
 def annonymous(view):
     @wraps(view)
     def wrapper(*arg,**kwargs):
@@ -43,18 +40,21 @@ def annonymous(view):
 @annonymous
 def login():
     form = LoginForm()
-    error = None
     if form.validate_on_submit():
-        user = db.validate_user(bcrypt_,form.username.data, form.password.data)
-
+        user = User.query.filter_by(username=form.username.data)
         if user != None:
-            login_user(user,form.remember.data)
-            flash('sign in successful', 'success')
-            return redirect(url_for('gallery.home'))
+            if bcrypt_.check_password_hash(user.password,form.password.data):
+                login_user(user,form.remember.data)
+                flash('sign in successful', 'success')
+                next=None
+                return redirect(url_for('gallery.home'))
+            else:
+                flash('invalid Password')
+                return redirect(url_for('auth.login'))
         else:
-            error = "invalid password"
+            flash('Invalid Username and Password')
 
-    return render_template('auth/signin.html', err = error, form=form)
+    return render_template('auth/signin.html', form=form)
         
 
 
@@ -63,27 +63,15 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        if db.get_user_by_username(form.username.data):
-            flash('username already exist', 'danger')
-            return redirect(url_for('auth.login'))
-        if db.get_user_by_email(form.email.data):
-            flash('Email already exist', 'danger')
-            return redirect(url_for('auth,login'))
-
-        passed,errors = db.validate_password(form.password.data)
-
-        if passed:
-            password = bcrypt_.generate_password_hash(form.password.data).decode('utf-8')
-            if db.create_user(name = form.name.data,username= form.username.data,email=form.email.data,
-                                        contact=form.contact.data,password=password):
-                flash('Account created successfully', 'success')
-                return redirect(url_for('gallery.home'))
-        
-        else:
-            return render_template('auth/signup.html', err=errors, form= form)
-
+        password = bcrypt_.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(name= form.name.data, username=form.username.data, email=form.email.data,contact = form.phone_no.data,password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created successfully', 'success')
+        return redirect(url_for('gallery.home'))    
 
     return render_template('auth/signup.html', form= form)
+
 
 
 @auth.route('/logout', methods=['GET','POST'])
@@ -96,12 +84,13 @@ def logout():
 
 @auth.route('/forgot/pasword/<username>')
 def request_password(username):
-    user = db.get_user_by_username(username)
+    user = User.query.filter_by(username=username).first()
     base_url = request.url_root
     response = ""
 
     if user:
-
+        #payload contains useful information which jwt will encrypt in the link to be sent
+        #exp == expiration date, iat == sent time
         payload = {
         "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2),
         "iat": datetime.datetime.utcnow(),
@@ -111,16 +100,18 @@ def request_password(username):
         if user.email and len(user.email) > 6:
             token = jwt.encode(payload,current_app.secret_key,algorithm="HS256")
             url = base_url + f"/auth/reset/password/token/{token}/{username}"
+
             message = f"""
-                    <p>Ignore if you did not expect this</p>
-                    <p>
-                        You have requested for a new password
-                        To reset your password click the link below
-                    </p>
-                    <a href="{url}">{url}</a>
-                    
-                    <p>Thank you.</p>
-            """
+                    <h2 style="background-color:#1e90ff; height:70px; width:250px; text-align:center; line-height:50%;">Sketch Password Reset <h2>
+                    <p style="padding:10px 0; line-height:1.5;">Simply ignore if you dont expect this email</p>
+                    <div style="padding:1rem; background-color:#ddd; line-height:1.5;">
+                        <p>
+                            You have requested for a new password
+                            To reset your password click the link below
+                        </p>
+                        <a style="text-decoration:none;" href="{url}">{url}</a>
+                    <h3 style="color:1e90ff;">Thank you.</h3>
+                """
 
             msg = Message("Sketch: Password Reset",html= message,
             sender=os.environ.get("Email"),recipients=[user.email])
@@ -142,7 +133,7 @@ def request_password(username):
         flash("no such user exist","danger")
         response ="No such user exist with the username provided"
 
-    return render_template("auth/password_message.html", response = response)
+    return render_template("auth/forgot_password.html", response = response)
 
             
 
@@ -150,26 +141,28 @@ def request_password(username):
 @auth.route("/reset/password/token/<token>/<username>")
 def password_change(token,username):
     try:
+        #jwt decode token from url
         payload = jwt.decode(token,current_app.secret_key,algorithms="HS256")
         user_id = payload["user_id"]
         
 
         errors = None
+        #if payload contains user_id
         if user_id:
-            user = db.get_user(user_id)
+            user = User.query.filter_by(id=user_id).first()
             if request.method == "POST":
                 password = request.form["password"]
-                passed,errors = db.validate_password(password)
-                if passed:
+                if len(password) > 7:
                     user.password = bcrypt_.generate_password_hash(password).decode('utf-8')
-                    maindb.session.commit()
-                    flash("Password reser successful","success")
+                    db.session.commit()
+                    flash("Password reset successful","success")
                     return render_template("auth/password.html")
-            
+        
         return render_template("auth/password.html", err=errors)
+    
     except jwt.ExpiredSignatureError:
         flash("This link has expired click on forget password again","danger")
-        return redirect(url_for("auth.register"))
+        return redirect(url_for("auth.login"))
     except jwt.InvalidTokenError or jwt.InvalidSignatureError:
         flash("Invalid link", "danger")
 
@@ -182,23 +175,21 @@ def password_reset():
     form = ChangePassword()
 
     if form.validate_on_submit():
-        user = db.get_user_by_username(current_user.username)
-        passed,errors= db.validate_password(form.password.data)
+        user = User.query.filter_by(username=current_user.username).first()
+        passed= bcrypt_.check_password_hash(user.password,form.password.data)
 
         if passed:
             user.password = bcrypt_.generate_password_hash(form.password.data).decode("utf-8")
-            maindb.session.commit()
+            db.session.commit()
             flash("password reset successful", "success")
             return redirect(url_for("gallery.home"))
-        else:
-            return render_template("auth/resetpassword.html", err=errors, form=form)
 
     return render_template("auth/resetpassword.html", form=form)
 
 
 @auth.route('/validusername/<username>')
 def valid_username(username):
-    if db.get_user_by_username(username):
+    if User.query.filter_by(username = username).first():
         return True
     return False
 
@@ -238,15 +229,18 @@ def oauth_callback(provider):
     social_id, username, email = oauth.callback()
 
     username_ = username
-    user = db.get_user_by_social_id(social_id)
+    user = User.query.filter_by(social_id=social_id).first()
     
-    username_pass = db.check_username(username)
+    username_pass = True if User.query.filter_by(username).first() else False
 
+    #if user exist in data base add a random number to username 
+    # then check again if such user exist. repeat if user exist
     while (username_pass == True):
         username =  username + str(randint(1,6000000))
-        username_pass = db.check_username(username)
+        username_pass = True if User.query.filter_by(username).first() else False
 
     if not user:
+        #if no user with such social id exists
         if email == None:
             email == ''
         
@@ -254,8 +248,9 @@ def oauth_callback(provider):
         # and we cant use either email,social_id or username to create password
         # this prevent someone from signing in knowing the username email and id
         user = User(social_id=social_id, name=username_ ,username=username, email=email,password=bcrypt_.generate_password_hash(secrets.token_hex(16)).decode('utf-8'))
-        maindb.session.add(user)
-        maindb.session.commit()
+        db.session.add(user)
+        db.session.commit()
+        
     login_user(user, True)
     flash('sign in successful', 'success')
     return redirect(url_for('gallery.home'))
--- main/auth/form.py
+++ main/auth/form.py
@@ -3,11 +3,9 @@ from wtforms import *
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flask_login import current_user
 
-from ..database import Database
 from ..models import User,Post,Notification,follow
 from .. import bcrypt_
 
-db = Database(User,Post,Notification,follow)
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(),Length(min=2,max=50)])
@@ -15,7 +13,9 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember')
 
     def validate_username(self, username):
-        if not db.get_user_by_username(username) or not db.get_user_by_email(username):
+        user = User.query.filter_by(username=username)
+        email = User.query.filter_by(email=username)
+        if not user or not email:
             raise ValidationError('Incorrect username or password')
 
 
@@ -23,19 +23,40 @@ class RegisterForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(),Length(min=3,max=50)])
     username = StringField('username', validators=[DataRequired(),Length(min=3,max=30)])
     email =  StringField('email', validators=[DataRequired(),Length(min=5,max=50),Email()])
-    contact = StringField('phone',validators=[Length(min=5,max=15)])
+    phone_no = StringField('phone',validators=[])
     password = PasswordField('password',validators=[DataRequired(), Length(min=7,max=30)])
     confirm_p = PasswordField('password',validators=[DataRequired(), Length(min=7,max=30),EqualTo('password')])
 
     def validate_username(self,username):
-        if db.get_user_by_username(username):
+        if User.query.filter_by(username=username).first():
             raise ValidationError('username already exist')
     
     def validate_email(self,email):
         if len(email) == 0:
             raise ValidationError('please enter an email')
-        if db.get_user_by_email(email):
+        if User.query.filter_by(email=email).first():
             raise ValidationError('email already exist')
+
+    def validate_phone_no(self,phone_no):
+        #if user input phone number then check if its less than five
+        if phone_no != '':
+            if len(phone_no) < 5:
+                raise ValidationError('Invalid phone number')
+    
+    def validate_password(self,password):
+        if password == None:
+            raise ValidationError('no password input')
+        
+        if len(password) < 7:
+            raise ValidationError('password must be up to 7 characters long')
+
+        if not any(n.isupper() for n in password):
+            raise ValidationError('password must contain capital letter')
+
+        if not any(n.isdigit() for n in password):
+            raise ValidationError('password must contain a number')
+
+
     
 class ChangePassword(FlaskForm):
     old_password = PasswordField("current password",validators=[DataRequired()]) 
@@ -43,6 +64,19 @@ class ChangePassword(FlaskForm):
     confirm_p = PasswordField('password',validators=[DataRequired(), Length(min=7,max=30),EqualTo('password')])
 
     def validate_old_password(self,old_password):
-        user = db.get_user_by_username(current_user.username)
+        user = User.query.filter_by(username= current_user.username).first()
         if not bcrypt_.check_password_hash(user.password,old_password):
             raise ValidationError("Invalid current password")
+    
+    def validate_password(self,password):
+        if password == None:
+            raise ValidationError('no password input')
+        
+        if len(password) < 7:
+            raise ValidationError('password must be up to 7 characters long')
+
+        if not any(n.isupper() for n in password):
+            raise ValidationError('password must contain capital letter')
+
+        if not any(n.isdigit() for n in password):
+            raise ValidationError('password must contain a number')
